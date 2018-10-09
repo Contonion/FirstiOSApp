1765d8d94d5a4b8eac48d4370da0674fdde09816
Cade Jackman <engineer202@outlook.com>
2018-10-09 08:44:16 -0600

    Added Model, and added a File

---
+++ FirstiOSApp/Model/ColorTool.swift
@@ -0,0 +1,13 @@
+//
+//  ColorTool.swift
+//  FirstiOSApp
+//
+//  Created by Jackman, Cade on 10/9/18.
+//  Copyright © 2018 CTEC. All rights reserved.
+//
+
+import Foundation
+import UIKit
+public class ColorTool {
+    
+}
